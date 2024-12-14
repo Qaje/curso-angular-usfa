b1ef918bd337da355d9f323e275d006eaa3d800f
qaje <javierpal3232@gmail.com>
2024-12-14 12:09:21 -0400

    change theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,29 @@
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   //content: ["./src/**/*.{html,js}"],
+//   // theme: {
+//   //   extend: {},
+//   // },
+//   daisyui: {
+//     themes: [{
+//       mytheme: {
+//                   "primary": "#fde047",
+//                   "secondary": "#4c1d95",
+//                           "accent": "#a21caf",
+//                           "neutral": "#fef9c3",
+//                           "base-100": "#f3e8ff",
+//                           "info": "#c7d2fe",
+//                           "success": "#86efac",
+//                           "warning": "#fde047",
+//                           "error": "#ef4444",
+//                   },
+//     }],
+//   },
+//   plugins: [
+//     require('daisyui'),
+//   ],
+// }
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
@@ -5,7 +31,7 @@ module.exports = {
     extend: {},
   },
   daisyui: {
-    themes: ["light", "dark", "cupcake"],
+    themes: ["light", "dark", "cupcake","luxury"],
   },
   plugins: [
     require('daisyui'),
